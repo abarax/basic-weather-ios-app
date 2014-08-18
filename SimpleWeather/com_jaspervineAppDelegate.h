7339bc51a050f5b2e8a38b2b36bfbeb8de3cbd9a
Leigh Appel <leigh@safesiteapp.com>
2014-08-18 17:50:34 +1000

    Initial Commit

---
+++ SimpleWeather/com_jaspervineAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  com_jaspervineAppDelegate.h
+//  SimpleWeather
+//
+//  Created by Leigh Appel on 18/08/2014.
+//  Copyright (c) 2014 JasperVine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface com_jaspervineAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleWeather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleWeather
+//
+//  Created by Leigh Appel on 18/08/2014.
+//  Copyright (c) 2014 JasperVine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "com_jaspervineAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([com_jaspervineAppDelegate class]));
+    }
+}
